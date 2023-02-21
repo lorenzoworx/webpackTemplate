5478b47aa68ebdea4eac27c91d74b480436ad12a
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-02-21 16:23:43 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-  mode:'development',
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'src'),
     },
 
-  plugins: [
-    new HtmlWebpackPlugin({
+    plugins: [
+      new HtmlWebpackPlugin({
         template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+      }),
     ],
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
   },
-}
 
 };
